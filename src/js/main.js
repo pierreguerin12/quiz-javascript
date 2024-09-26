ca074ff15bc2e6f9a5a26b3282e332a24c7e4217
pierreguerin12 <pierre.guerin@studentfr.ch>
2024-09-26 16:44:59 +0200

    final

--- src/js/main.js
+++ src/js/main.js
@@ -5,26 +5,29 @@
 
 // 1. Expliquez ce qu'est-ce qu'une variable.
 // REPONSE:
-
+Une variable est un moyen de stocker une information afin de la réutiliser ou de la manipuler.
 // -----------------------
 // 2. Déclarez une variable s'appelant "firstName" et dont la valeur est votre prénom.
-
+const age = 24;
+const name = "Pierre";
+console.log(age); // -> 24
+console.log(name); // -> Pierre
 // -----------------------
 // 3. Déclarez une variable s'appelant "age" et dont la valeur est votre âge. Attention, sa valeur pourrait être modifiée par la suite.
-
+let age = 24;
+console.log(age)
 // -----------------------
 // 4. Dans le code HTML (lignes 17 - 23), citez les divers éléments composant l'élément HTML <input>:
-// - Balise HTML:
-// - Classe(s):
-// - Attribut(s):
+// - Balise HTML:type, name, placeholder, id, class
+// - Classe(s): id, class
+// - Attribut(s):card-input, password-input, text
 
 // -----------------------
 // 5. Expliquez la différence entre une class et un ID.
-// REPONSE:
-
+// REPONSE: l'id est unique au document pad forcément la classe
 // -----------------------
 // 6. A quoi sert la méthode document.querySelector("..."); ?
-// REPONSE:
+// REPONSE: à sélectionner ce que l'on veut dans le html/css
 
 // -----------------------
 // 7. A l'aide de la méthode document.querySelector("..."), sélectionnez l'élément <p> (ligne 14 du code HTML):
@@ -40,7 +43,7 @@
 
 // -----------------------
 // 10. Expliquez ce qu'est un écouteur d'événement.
-// REPONSE:
+// REPONSE: là je sui vraiment désolé c'est un peu trop compliqué pour moi
 
 // -----------------------
 // 11. Sur l'élément HTML button (ligne 26 du code HTML), écoutez l'événement "click":
